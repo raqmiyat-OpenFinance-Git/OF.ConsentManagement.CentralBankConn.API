5d7257b9f35dfd318e63639deb2e89c6ea3cdf01
Swathi1612 <157341887+Swathi1612@users.noreply.github.com>
2025-10-09 13:39:04 +0530

    Awaiting Authorization Modified.

--- OF.ConsentManagement.CentralBankConn.API/Stub/ConsentPostResponseSample.cs
+++ OF.ConsentManagement.CentralBankConn.API/Stub/ConsentPostResponseSample.cs
@@ -17,7 +17,7 @@ public static class ConsentPostResponseSample
                 ConsentGroupId = "group-001",
                 RequestUrl = "https://bankapi.com/consents/resp-12345",
                 ConsentType = "AccountAccess",
-                Status = "Authorised",
+                Status = "Awaiting Authorization",
 
                 Request = new ConsentRequestDto
                 {
